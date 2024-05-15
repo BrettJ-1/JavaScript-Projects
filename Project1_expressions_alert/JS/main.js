135d6e01b5c2694fb3a8b7a6fc8d4f139dbff6b7
BrettJ-1 <brettjones415@gmail.com>
2024-05-15 12:56:58 -0700

    JavaScript Examples

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello, World!");
+document.write("Hello, World!");
+var A = "This is a string";
+document.write(A);
+var B = "This is another string";
+window.alert(B);
+
+document.write("My favorite saying, \"Don't be scared, be prepared\" is the best.");
+document.write("This is going to be an example of how I can"
++ " use JavaScript to concatenate text");

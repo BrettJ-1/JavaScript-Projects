50343e9df58cdb7330bda59c12e07b8844bb06f3
BrettJ-1 <brettjones415@gmail.com>
2024-05-16 10:11:09 -0700

    Finalized Changes

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,24 +1,26 @@
-window.alert("Hello, World!");
+window.alert("Hello, World!"); //Creating an alert//
 
-document.write("Hello, World!");
+document.write("Hello, World!"); //Displaying Text//
 
-var A = "This is a string";
+var A = "This is a string"; //Creating a variable to write text//
 document.write(A);
 
-var B = "This is another string";
+var B = "This is another string"; //Creating a variable to create an alert//
 window.alert(B);
 
-document.write("My favorite saying, \"Don't be scared, be prepared\" is the best.");
+document.write("My favorite saying, \"Don't be scared, be prepared\" is the best."); //Writing quote//
 
-document.write("This is going to be an example of how I can"
+document.write("This is going to be an example of how I can" //Concatenating Text//
     + " use JavaScript to concatenate text");
 
-var B = "Concatenated" + " String"
+var B = "Concatenated" + " String" //Creating a variable to create a concatenated string//
 document.write(B)
 
-var Characters = "The Avengers", Tony_Stark = "Iron Man", Steve_Rogers = "Captain America",
-Bruce_Banner = "Hulk", Clint_Barton =
-"Hawkeye";
-document.write (Tony_Stark);
+var Characters = "The Avengers", Tony_Stark = "Iron Man", Steve_Rogers = "Captain America", //Write out a chosen variable//
+    Bruce_Banner = "Hulk", Clint_Barton =
+        "Hawkeye";
+document.write(Tony_Stark);
+
+document.write(3 + 4); //Writing out a function//
 
 
